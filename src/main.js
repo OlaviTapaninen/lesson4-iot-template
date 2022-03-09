72da96372ba2f43911a07df46371543c7a8d7170
Tapaninen Olavi <olavi.tapaninen@edu.espoo.fi>
2022-03-09 14:03:05 +0200

    added config.json

--- src/main.js
+++ src/main.js
@@ -1,15 +1,18 @@
 const ruuvi = require('node-ruuvitag');
 const fetch = require('node-fetch');
 
+const CONFIG = require('../config.json');
+
 ruuvi.on('found', (tag) => {
   console.log(`Found RuuviTag with id: ${tag.id}`);
 
   tag.on('updated', (data) => {
     console.log(`${tag.id}: ${JSON.stringify(data)}`);
 
-    const body = { message: `temperature: ${data.temperature}` };
 
-    fetch('http://95.216.154.69:9000/api/chats', {
+    const body = { temperature: data.temperature, huminidy: data.huminidy, sensor_id: `${tag.id}` };
+
+    fetch(CONFIG['api-url'], {
       method: 'post',
       body: JSON.stringify(body),
       headers: { 'Content-Type': 'application/json' },
